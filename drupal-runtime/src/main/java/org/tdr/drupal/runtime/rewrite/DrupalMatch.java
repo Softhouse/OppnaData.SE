565ad2068b4682c5526a08d35660ecdaa49c7188
Niclas Cedermalm <niclas@cedermalm.se>
2012-11-20 23:08:58 +0100

    Corrected runtimes

---
+++ drupal-runtime/src/main/java/org/tdr/drupal/runtime/rewrite/DrupalMatch.java
@@ -0,0 +1,39 @@
+
+package org.tdr.drupal.runtime.rewrite;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.tuckey.web.filters.urlrewrite.extend.RewriteMatch;
+import org.tuckey.web.filters.urlrewrite.utils.Log;
+
+public class DrupalMatch extends RewriteMatch {
+	
+    private static Log log = Log.getLog(DrupalMatch.class);
+
+    /* (non-Javadoc)
+     * @see org.tuckey.web.filters.urlrewrite.extend.RewriteMatch#execute(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    public boolean execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+        String queryString = request.getQueryString();
+        
+        String contextstr = request.getContextPath();
+        String requestUri = request.getRequestURI();
+        String drupalpath = requestUri.replaceAll(contextstr, "").substring(1);
+        StringBuilder newURI = new StringBuilder(512);
+        
+        newURI.append("/index.php?q=").append(drupalpath);
+        
+        if( queryString != null ) {
+            newURI.append("&").append(request.getQueryString());
+        }
+        log.debug("New URI = "+newURI.toString());
+        RequestDispatcher rd = request.getRequestDispatcher(newURI.toString());
+        rd.forward(request, response);
+        return true;
+    }
+}
---
+++ drupal-runtime/src/main/java/org/tdr/drupal/runtime/rewrite/DrupalRule.java
@@ -0,0 +1,56 @@
+package org.tdr.drupal.runtime.rewrite;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.tuckey.web.filters.urlrewrite.extend.RewriteMatch;
+import org.tuckey.web.filters.urlrewrite.extend.RewriteRule;
+import org.tuckey.web.filters.urlrewrite.utils.Log;
+ 
+
+public class DrupalRule extends RewriteRule {
+	
+    private static Log log = Log.getLog(DrupalRule.class);
+
+    private ServletContext sc;
+    
+    public boolean init(ServletContext sc) {
+        this.sc = sc;
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see org.tuckey.web.filters.urlrewrite.extend.RewriteRule#matches(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    public RewriteMatch matches(HttpServletRequest request, HttpServletResponse response)
+    {
+        String requestURI = request.getRequestURI();
+        if ( requestURI == null ) {
+            return null;
+        }
+        
+        if ( requestURI.equals(request.getContextPath()) ) {
+            return null;
+        }
+        
+        requestURI = requestURI.replaceAll(request.getContextPath(), "");
+        log.debug("@DRUPALRULE: RequestUri = "+requestURI);
+        
+        if ( requestURI.startsWith("/misc") || 
+        	 requestURI.startsWith("/modules") ||
+        	 requestURI.startsWith("/themes") ||
+        	 requestURI.startsWith(("/sites")) ) {
+            return null;
+        }
+        
+        String realPath = sc.getRealPath(requestURI);
+        log.debug("@DRUPALRULE: File check real path = " + realPath);
+        if ( realPath != null) { // resource exists -> skip resolving to Drupal page URI
+                return null;
+        }
+
+        log.debug("@DRUPALRULE: DrupalMatch for requestURI = "+requestURI);
+        return new DrupalMatch();
+    }
+}
