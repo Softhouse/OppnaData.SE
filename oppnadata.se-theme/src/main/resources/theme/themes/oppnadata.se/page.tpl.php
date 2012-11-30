18d8eb4e503f9d64d10cc5b08010c59567be0124
Joakim Bengtson <joakim.bengtson@gmail.com>
2012-11-30 16:59:37 +0100

    Mostly removing unnecessary code and adjusting columns for tablet

--- oppnadata.se-theme/src/main/resources/theme/themes/oppnadata.se/page.tpl.php
+++ oppnadata.se-theme/src/main/resources/theme/themes/oppnadata.se/page.tpl.php
@@ -4,123 +4,99 @@
 
 <!DOCTYPE html>
 <html lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-<head>
-    <meta charset="utf-8" />
-    <link href='http://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400italic,400,700' rel='stylesheet' type='text/css' />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <?php print $head; ?>
-    <title><?php print $head_title; ?></title>
-    <?php print $styles; ?>
-    <?php print $scripts; ?>
-    <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400italic,400,700' rel='stylesheet' type='text/css' />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <?php print $head; ?>
+        <title><?php print $head_title; ?></title>
+        <?php print $styles; ?>
+        <?php print $scripts; ?>
+        <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+    </head>
 
 
-<body class="<?php print $body_classes; ?>">
+    <body class="<?php print $body_classes; ?>">
 
 
-    <header>
-        <div id="header-container">
+        <header>
+            <div id="header-container">
+                <h1>&Ouml;ppnadata</h1>
 
-            <div id="logo">
+                <div id="header-right">
+                    <?php print $header_right; ?>
+                </div>
 
-                <?php if (!empty($logo)): ?>
-                    <a href="<?= $front_page; ?>" title="<?= t('Home'); ?>" rel="home">
-                        <img src="<?= $logo; ?>" alt="<?= t('Home'); ?>" />
-                    </a>
-                <?php endif; ?>
-
+                <div id="nav-container">
+                    <nav id="nav">
+                        <?= $nav; ?>
+                    </nav>
+                </div>
             </div>
+        </header>
 
-            <div id="logo-mobile">
-                <?php if (!empty($logo)): ?>
-                    <a href="<?= $front_page; ?>" title="<?= t('Home'); ?>" rel="home">
-                        <img src="<?= $logo_mobile; ?>" alt="<?= t('Home'); ?>" />
-                    </a>
-                <?php endif; ?>
+        <section>
+            <div id="main">
+                <?php if (!empty($breadcrumb)): ?><div id="breadcrumbs"><?= $breadcrumb; ?></div><?php endif; ?>
+
+                <?php if (!empty($mission)): ?><div id="mission"><?= $mission; ?></div><?php endif; ?>
+
+                <?php if (!empty($title)): ?><h1 id="page-title"><?= $title; ?></h1><?php endif; ?>
+
+                <?php if (!empty($tabs)): ?><div class="tabs"><?= $tabs; ?></div><?php endif; ?>
+
+                <?php if (!empty($messages)): print $messages; endif; ?>
+
+                <?php if (!empty($help)): print $help; endif; ?>
+
+                <div id="content">
+
+                    <?= $content; ?>
+
+                </div>
+
+                <aside>
+
+                    <?= $right; ?>
+
+                </aside>
             </div>
+        </section>
 
-            <div id="header-right">
-                <?php print $header_right; ?>
-            </div>
+        <footer>
+            <div id="footer-container">
+                <?= $footer_message; ?>
 
-            <div id="nav-container">
-                <nav id="nav">
-                    <?= $nav; ?>
+                <?php if (!empty($footer)): print $footer; endif; ?>
+
+                <nav class="<?php if (!empty($primary_links)) { print "withprimary"; } if (!empty($secondary_links)) { print " withsecondary"; } ?> ">
+                    <?php if (!empty($primary_links)): ?>
+
+                    <div id="primary" class="clear-block">
+                        <?= theme('links', $primary_links, array('class' => 'links primary-links')); ?>
+
+                    </div>
+                    <?php endif; ?>
+
+                    <?php if (!empty($secondary_links)): ?>
+
+                    <div id="secondary" class="clear-block">
+                        <?= theme('links', $secondary_links, array('class' => 'links secondary-links')); ?>
+
+                    </div>
+                    <?php endif; ?>
+
                 </nav>
             </div>
-        </div>
-    </header>
-
-    <section>
-        <div id="content">
-
-            <?php if (!empty($breadcrumb)): ?><div id="breadcrumb"><?= $breadcrumb; ?></div><?php endif; ?>
-
-            <?php if (!empty($mission)): ?><div id="mission"><?= $mission; ?></div><?php endif; ?>
+        </footer>
+        <?= $closure; ?>
 
 
-            <?php if (!empty($title)): ?><h1 id="page-title"><?= $title; ?></h1><?php endif; ?>
+        <?php
+            // print '<pre>';
+            // var_dump(get_defined_vars());
+            // print '</pre>';
+        ?>
 
-            <?php if (!empty($tabs)): ?><div class="tabs"><?= $tabs; ?></div><?php endif; ?>
-
-            <?php if (!empty($messages)): print $messages; endif; ?>
-
-            <?php if (!empty($help)): print $help; endif; ?>
-
-            <div id="content-content" class="clear-block">
-
-                <?= $content; ?>
-
-            </div>
-
-            <aside id="sidebar-right" class="column sidebar">
-
-                <?= $right; ?>
-
-            </aside>
-        </div>
-    </section>
-
-    <footer>
-        <div id="footer-container">
-            <?= $footer_message; ?>
-
-            <?php if (!empty($footer)): print $footer; endif; ?>
-
-            <nav class="<?php if (!empty($primary_links)) { print "withprimary"; } if (!empty($secondary_links)) { print " withsecondary"; } ?> ">
-                <?php if (!empty($primary_links)): ?>
-
-                <div id="primary" class="clear-block">
-                    <?= theme('links', $primary_links, array('class' => 'links primary-links')); ?>
-
-                </div>
-                <?php endif; ?>
-
-                <?php if (!empty($secondary_links)): ?>
-
-                <div id="secondary" class="clear-block">
-                    <?= theme('links', $secondary_links, array('class' => 'links secondary-links')); ?>
-
-                </div>
-                <?php endif; ?>
-
-            </nav>
-
-            <img src="/sites/default/files/regering.png" alt="" id="bottomImg" />
-
-        </div>
-    </footer>
-    <?= $closure; ?>
-
-
-    <?php
-        // print '<pre>';
-        // var_dump(get_defined_vars());
-        // print '</pre>';
-    ?>
-
-</body>
-
-
+    </body>
 </html>
